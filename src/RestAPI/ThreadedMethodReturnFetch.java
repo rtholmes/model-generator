f17da9db1497c4e9a06d6dd24d37dd1b05d7e5bd
siddhukrs <siddhukrs@localhost>
2014-03-18 21:26:41 -0400

    no more unecessary return type fetches, replaced with placeholder

--- src/RestAPI/ThreadedMethodReturnFetch.java
+++ src/RestAPI/ThreadedMethodReturnFetch.java
@@ -2,7 +2,10 @@ package RestAPI;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.json.JSONObject;
 
 import com.google.common.collect.HashMultimap;
@@ -12,24 +15,34 @@ import Node.NodeJSON;
 
 public class ThreadedMethodReturnFetch implements Runnable 
 {
+	private ASTNode treeNode;
 	private GraphServerAccess model;
 	private NodeJSON methodNode;
 	private HashMap<NodeJSON, NodeJSON> methodReturnCache;
 	private HashMultimap<ArrayList<Integer>, NodeJSON> candidateAccumulator;
 	private ArrayList<Integer> scopeArray;
-	
+	private HashSet<Integer> interestingNodes;
 
 	public ThreadedMethodReturnFetch(NodeJSON candidateMethodNode,
 			HashMap<NodeJSON, NodeJSON> methodReturnCache,
 			HashMultimap<ArrayList<Integer>, NodeJSON> candidateAccumulator,
 			ArrayList<Integer> scopeArray, 
-			GraphServerAccess graphModel) 
+			GraphServerAccess graphModel,
+			ASTNode treeNode) 
 	{
 		this.methodNode = candidateMethodNode;
 		this.methodReturnCache = methodReturnCache;
 		this.candidateAccumulator = candidateAccumulator;
 		this.scopeArray = scopeArray;
 		this.model = graphModel;
+		this.treeNode = treeNode;
+		interestingNodes = new HashSet<Integer>();
+		interestingNodes.add(7);	//assignment
+		interestingNodes.add(32);	//methodinvocation
+		interestingNodes.add(59); 	//VARIABLE_DECLARATION_FRAGMENT
+		interestingNodes.add(42); 	//SIMPLE_NAME
+		interestingNodes.add(23); 	//FIELD_DECLARATION
+		//interestingNodes.add(21);	//method parameter(expression_statement)
 	}
 
 	@Override
@@ -40,10 +53,29 @@ public class ThreadedMethodReturnFetch implements Runnable
 	
 	public void run()
 	{
-		NodeJSON retElement = model.getMethodReturn(methodNode, methodReturnCache);
-		if(retElement!=null)
+		Integer parentType = treeNode.getParent().getNodeType();
+		if(!interestingNodes.contains(parentType))
 		{
+			System.out.println("PlaceHolder " + methodNode.getProperty("id") + "-" + treeNode.getParent().getNodeType());
+			
+			JSONObject data = new JSONObject();
+			data.put("id", "MyRTPlaceHolderP.MyRTPlaceHolderEN");
+			data.put("exactName", "MyRTPlaceHolderEN");
+			JSONObject obj = new JSONObject();
+			obj.put("data", data);
+			
+			NodeJSON retElement = new NodeJSON(obj);
 			candidateAccumulator.put(scopeArray, retElement);
+			methodReturnCache.put(methodNode, retElement);
+		}
+		else
+		{
+			NodeJSON retElement = model.getMethodReturn(methodNode, methodReturnCache);
+			if(retElement!=null)
+			{
+				System.out.println("FromModel " + methodNode.getProperty("id") + "-" + treeNode.getParent().getNodeType());
+				candidateAccumulator.put(scopeArray, retElement);
+			}
 		}
 		
 	}
