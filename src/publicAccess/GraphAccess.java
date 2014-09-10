604328ae6b1bce60b49a691be8b448b05e3f5de7
siddhukrs <siddhukrs@localhost>
2014-09-10 22:20:22 +0530

    checks if class starts with uppercase after package name ends

--- src/publicAccess/GraphAccess.java
+++ src/publicAccess/GraphAccess.java
@@ -26,65 +26,80 @@ public class GraphAccess
 		//(1) Given a package name, what classes does the Oracle know about? 
 		//(2) Given a class (FQN), what methods does it contain? and 
 		//(3) Given a method identifier, which classes (FQNs) declare a method with that name? 
-		
+
 		int queryType = Integer.valueOf(args[0]);
 		String query = args[1];
 		graphDb = new GraphServerAccess(DB_URI);
 
 		long startTime = System.nanoTime();
-		
+
 		JSONArray jsonArray = new JSONArray();
 		JSONObject returnValue = new JSONObject();
-		
+
 		switch(queryType)
 		{
-			case 1:
+		case 1:
+		{
+			IndexHits<NodeJSON> classes = graphDb.getClassesInPackage_PUBLIC_ACCESS(query);
+			for(final NodeJSON classNode : classes)
 			{
-				IndexHits<NodeJSON> classes = graphDb.getClassesInPackage_PUBLIC_ACCESS(query);
-				for(final NodeJSON classNode : classes)
+				String className = classNode.getProperty("id");
+				if(isRightPackage(query, className))
 				{
 					JSONObject value = classNode.getJSONObject().getJSONObject("data");
 					jsonArray.put(value);
 				}
-				break;
 			}
-			case 2:
-			{
-				IndexHits<NodeJSON> methods = graphDb.getMethodsInClass_PUBLIC_ACCESS(query);
-				for(final NodeJSON methodNode : methods)
-				{
-					JSONObject value = methodNode.getJSONObject().getJSONObject("data");
-					jsonArray.put(value);
-				}
-				break;
-			}
-			case 3:
-			{
-				IndexHits<NodeJSON> methods = graphDb.getCandidateMethodNodes(query, new HashMap<String, IndexHits<NodeJSON>>());
-				for(NodeJSON methodNode : methods)
-				{
-					JSONObject value = methodNode.getJSONObject().getJSONObject("data");
-					String methodName = (String) value.get("id");
-					value.put("className",extractClassName(methodName));
-					jsonArray.put(value);
-				}
-				break;
-			}
-			default:
-			{
-				System.out.println("Invalid option");
-			}
+			break;
 		}
-		
+		case 2:
+		{
+			IndexHits<NodeJSON> methods = graphDb.getMethodsInClass_PUBLIC_ACCESS(query);
+			for(final NodeJSON methodNode : methods)
+			{
+				JSONObject value = methodNode.getJSONObject().getJSONObject("data");
+				jsonArray.put(value);
+			}
+			break;
+		}
+		case 3:
+		{
+			IndexHits<NodeJSON> methods = graphDb.getCandidateMethodNodes(query, new HashMap<String, IndexHits<NodeJSON>>());
+			for(NodeJSON methodNode : methods)
+			{
+				JSONObject value = methodNode.getJSONObject().getJSONObject("data");
+				String methodName = (String) value.get("id");
+				value.put("className",extractClassName(methodName));
+				jsonArray.put(value);
+			}
+			break;
+		}
+		default:
+		{
+			System.out.println("Invalid option");
+		}
+		}
+
 		long endTime = System.nanoTime();
 		double time = (double)(endTime-startTime)/(1000000000);
 		//returnValue.put("time", time);
 		returnValue.put("api_elements", jsonArray);
 		//returnValue.put("count", jsonArray.length());
 		System.out.println(returnValue.toString(3));
-	}
 	
-	public static String extractClassName(String methodName)	//to store class name and exact method name as ivars
+		}
+
+	private static boolean isRightPackage(String packageName, String className) 
+	{
+		//int index = className.indexOf(packageName + ".");
+		int index = 0;
+		if(Character.isUpperCase(className.charAt(index + packageName.length()+1)))
+			return true;
+		else
+			return false;
+	}
+
+	private static String extractClassName(String methodName)	//to store class name and exact method name as ivars
 	{
 		if (methodName.endsWith("<clinit>"))
 		{
@@ -99,7 +114,7 @@ public class GraphAccess
 			for (int i=1; i<array.length-1; i++) 
 				className += "." + array[i];
 			return className;
-	 	}
-		
+		}
+
 	}
 }
